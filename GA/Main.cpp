e3101d7f22025080368e20fbf2b73055ac892fbd
guoze.lin <woodlgz@gmail.com>
2013-04-15 07:32:29 -0400

    GA

---
+++ GA/Main.cpp
@@ -0,0 +1,15 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Apr 15, 2013
+ *      Author: woodlgz
+ */
+
+#include <iostream>
+#include "GA.h"
+
+using namespace std;
+
+int main(int argc,char** argv){
+	return 0;
+}
